a11452006d35c008d138e9cf11b28af5115d2b16
Dobrunia <dobkos@hotmail.com>
2023-09-10 13:08:25 +0300

    nav

--- src/http/api.ts
+++ src/http/api.ts
@@ -22,11 +22,13 @@ $api.interceptors.response.use(
   },
   async (error) => {
     const originalRequest = error.config;
+    console.log(error)
     if (
       error.response.status == 401 &&
       error.config &&
       !error.config._isRetry
     ) {
+      
       originalRequest._isRetry = true;
       try {
         const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import './style.css';
+import './styles/navigation.css';
 import './registration-authorization/reg_auth.css';
 import { isUserLoggedIn, userIn, userOut } from './services/main-page-service';
 
-if (isUserLoggedIn()) {
-  userIn();
-} else {
-  userOut();
-}
+// if (isUserLoggedIn()) {
+//   userIn();
+// } else {
+//   userOut();
+// }
