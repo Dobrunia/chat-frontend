6d6ee79c923afad3b1d7c87b7a86ce6f05fca2ec
Dobrunia <dobkos@hotmail.com>
2023-11-10 08:59:18 +0300

    var(--navDarkBg)

--- src/services/main-page-service.js
+++ src/services/main-page-service.js
@@ -489,10 +489,10 @@ async function renderProfilePage(userDATA) {
     target="_blank"
     title="Ссылка на телеграм ${userDATA.username}"
     ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="50px" height="50px">
-    <path fill="#141414" d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z" />
+    <path fill="var(--navDarkBg)" d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z" />
     <path fill="var(--attention)" d="M33.95,15l-3.746,19.126c0,0-0.161,0.874-1.245,0.874c-0.576,0-0.873-0.274-0.873-0.274l-8.114-6.733 l-3.97-2.001l-5.095-1.355c0,0-0.907-0.262-0.907-1.012c0-0.625,0.933-0.923,0.933-0.923l21.316-8.468 c-0.001-0.001,0.651-0.235,1.126-0.234C33.667,14,34,14.125,34,14.5C34,14.75,33.95,15,33.95,15z" />
     <path fill="var(--attention)" d="M23,30.505l-3.426,3.374c0,0-0.149,0.115-0.348,0.12c-0.069,0.002-0.143-0.009-0.219-0.043 l0.964-5.965L23,30.505z" />
-    <path fill="#141414" d="M29.897,18.196c-0.169-0.22-0.481-0.26-0.701-0.093L16,26c0,0,2.106,5.892,2.427,6.912 c0.322,1.021,0.58,1.045,0.58,1.045l0.964-5.965l9.832-9.096C30.023,18.729,30.064,18.416,29.897,18.196z" />
+    <path fill="var(--navDarkBg)" d="M29.897,18.196c-0.169-0.22-0.481-0.26-0.701-0.093L16,26c0,0,2.106,5.892,2.427,6.912 c0.322,1.021,0.58,1.045,0.58,1.045l0.964-5.965l9.832-9.096C30.023,18.729,30.064,18.416,29.897,18.196z" />
   </svg>
   </a>`;
     $('#telegramLink_input').value = userDATA.telegramLink;
@@ -506,8 +506,8 @@ async function renderProfilePage(userDATA) {
     title="Ссылка на steam ${userDATA.username}"
     ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="50px" height="50px">
     <linearGradient id="wI0QYw54EXg5_LlgK1HXYa" x1="24" x2="24" y1="44" y2="4" gradientUnits="userSpaceOnUse">
-      <stop offset="0" stop-color="#141414" />
-      <stop offset="1" stop-color="#141414" />
+      <stop offset="0" stop-color="var(--navDarkBg)" />
+      <stop offset="1" stop-color="var(--navDarkBg)" />
     </linearGradient>
     <path fill="url(#wI0QYw54EXg5_LlgK1HXYa)" d="M44,24c0,11.04-8.96,20-20,20c-8.39,0-15.57-5.16-18.53-12.48c-0.09-0.2-0.16-0.4-0.24-0.6 c-0.07-0.19-0.14-0.39-0.2-0.59C4.36,28.34,4,26.21,4,24c0-0.17,0-0.35,0.02-0.52c-0.01-0.17,0-0.35,0.01-0.52 C4.57,12.4,13.3,4,24,4C35.04,4,44,12.95,44,24z" />
     <circle cx="28.5" cy="18.5" r="3.5" fill="var(--attention)" />
