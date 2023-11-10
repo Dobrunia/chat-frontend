0d88aaea5fd3e3e88a3c1acafc85570c24dd8ed5
Dobrunia <dobkos@hotmail.com>
2023-11-10 19:14:48 +0300

    checkPasswordSecurity

--- src/registration-authorization/validation.ts
+++ src/registration-authorization/validation.ts
@@ -1,5 +1,26 @@
 import { FormValidationType, ValidationType } from '../models/types.js';
 
+function checkPasswordSecurity(password: string | undefined) {
+  if (!password) {
+    return 'Пароля нет';
+  }
+  var length = password.length;
+  var hasUppercase = /[A-Z]/.test(password);
+  var hasLowercase = /[a-z]/.test(password);
+  var hasNumber = /[0-9]/.test(password);
+  var hasSpecialChar = /[^a-zA-Z\d]/.test(password);
+
+  if (length < 8) {
+    return 'Пароль слишком короткий';
+  }
+
+  if (!hasUppercase || !hasLowercase || !hasNumber || !hasSpecialChar) {
+    return 'Пароль должен содержать как минимум одну заглавную букву на английском языке, одну строчную на английском языке, одну цифру и один специальный символ.';
+  }
+
+  return false;
+}
+
 /**
  * Функция валидации формы
  * @param data
@@ -11,6 +32,9 @@ export function validation(data: FormValidationType, type: ValidationType) {
     }
     if (data.password !== data.password2) {
       return 'Пароли не совпадают';
+    }
+    if (checkPasswordSecurity(data.password)) {
+      return checkPasswordSecurity(data.password);
     }
   }
   const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
