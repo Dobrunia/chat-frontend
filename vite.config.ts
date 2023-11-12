6d5a5da1672a5ce601dbcebc257da9f3029c8741
Dobrunia <dobkos@hotmail.com>
2023-11-12 05:58:16 +0300

    get rdy

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import posthtml from '@vituum/vite-plugin-posthtml';
+
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+
+export default defineConfig({
+  root,
+  plugins: [posthtml()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        text: resolve(root, 'test', 'test.html'),
+      },
+    },
+  },
+});
